dbc0cfdc0a13426705bb7e451b59d5aac40fef91
pyyoek <joelnrlson@gmai.com>
2024-04-12 15:53:36 +0300

    food page and drinks fetched succesfully

--- lib/drinks.dart
+++ lib/drinks.dart
@@ -2,15 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class Foods {
-  final List<dynamic> foodname;
-  final List<dynamic> img;
-  final List<dynamic> id;
+class Drinks {
+  final List<dynamic> drinkname;
+  // final List<dynamic> img;
+  final List<int> id;
   final List<dynamic> price;
 
-  Foods ({
-    required this.foodname,
-    required this.img,
+  Drinks ({
+    required this.drinkname,
+    // required this.img,
     required this.id,
     required this.price,
   });
@@ -19,25 +19,58 @@ class Foods {
 class MyDrinks extends StatelessWidget {
   const MyDrinks({super.key});
 
-  Future<Foods> getFoods() async {
+  Future<Drinks> getDrinks() async {
     String uri = "http://127.0.0.1:8000/api/drinks";
     final response = await http.get(Uri.parse(uri));
     List<dynamic> food = [];
-    List<dynamic> ids = [];
+    List<int> ids = [];
     List<dynamic> prices = [];
-    List<dynamic> imgs = [];
+    // List<dynamic> imgs = [];
     if (response.statusCode == 200){
-      food.add(json.decode(response.body)['foodname']);
-      print(food);
+      final results = json.decode(response.body);
+      for (dynamic i in results) {
+        food.add(i['drinkname']);
+        ids.add(i['id']);
+        prices.add(i['price']);
+      }
+      print([prices]);
     }
-      return Foods(foodname: food, id: ids, price: prices, img: imgs);
+      return Drinks(drinkname: food, id: ids, price: prices);
+      // return Foods(foodname: food, id: ids, price: prices, img: imgs);
   }
 
   @override 
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => {getFoods()},
-      child: Text("illo"),
+    return FutureBuilder<Drinks>(
+      future: getDrinks(), 
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }else if(snapshot.hasError){
+          return Center(
+            child: Column(
+              children: [
+                Text("Error: ${snapshot.error}"),
+                SizedBox(height: 10,),
+                CircularProgressIndicator()
+              ],
+            ),
+          );
+        }else{
+          Drinks? drinks = snapshot.data;
+          return ListView.builder(
+            itemCount: drinks!.drinkname.length,
+            itemBuilder: (context, index) {
+              return Column(
+                children: [
+                  Text(drinks.drinkname[index]),
+                  Text(drinks.price[index]),
+                ],
+              );
+            }
+            );
+        }
+      }
     );
   }
 }
